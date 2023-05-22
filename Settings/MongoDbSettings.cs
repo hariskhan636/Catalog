57e400ca58d36e7fba0634ff3f621edfdbe79f5f
haris.khan636 <harisjriv@gmail.com>
2023-05-22 17:22:53 +0500

    Added Secret Manager

--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -5,12 +5,16 @@ namespace Catalog.Settings
         public string Host { get; set; }
         public string Port { get; set; }
 
+        public string User { get; set; }
+
+        public string Password { get; set; }
         public string ConnectionString
         {
             get
             {
-                return $"mongodb://{Host}:{Port}";
+                return $"mongodb://{User}:{Password}@{Host}:{Port}";
             }
         }
+
     }
 }
