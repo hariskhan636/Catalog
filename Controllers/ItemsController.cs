6463a4d0b1ae87a22c193fc95c5a636e381e3c97
haris.khan636 <harisjriv@gmail.com>
2023-05-24 12:48:20 +0500

    Updated controller

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -20,9 +20,15 @@ namespace Catalog.Controllers
         }
 
         [HttpGet]
-        public async Task<IEnumerable<ItemDto>> GetItemsAsync()
+        public async Task<IEnumerable<ItemDto>> GetItemsAsync(string name = null)
         {
             var items = (await repository.GetItemsAsync()).Select(item => item.AsDto());
+
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+
+                items = items.Where(item => item.Name.Contains(name, StringComparison.OrdinalIgnoreCase));
+            }
 
             logger.LogInformation($"{DateTime.UtcNow.ToString("hh:mm:ss")}: Retrieved: {items.Count()} items ");
 
@@ -94,6 +100,7 @@ namespace Catalog.Controllers
             return NoContent();
 
         }
+
     }
 
 }
